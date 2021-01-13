7dbfec53548fdea9497259bb4d51df6c0c65695d
A9G-Data-Droid <akauffman@cmm-measure.com>
2021-01-13 08:37:15 -0800

    Just the module code
    Initial commit.
    This code has been translated and tested.

---
+++ FastDirectoryEnumerator.vb
@@ -0,0 +1,295 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.IO
+Imports System.Runtime.ConstrainedExecution
+Imports System.Runtime.InteropServices
+Imports System.Security.Permissions
+Imports Microsoft.Win32.SafeHandles
+
+
+
+<Serializable>
+Public Class FileData
+    Public ReadOnly Attributes As FileAttributes
+
+    Public ReadOnly Property CreationTime As DateTime
+        Get
+            Return Me.CreationTimeUtc.ToLocalTime()
+        End Get
+    End Property
+
+    Public ReadOnly CreationTimeUtc As DateTime
+
+    Public ReadOnly Property LastAccesTime As DateTime
+        Get
+            Return Me.LastAccessTimeUtc.ToLocalTime()
+        End Get
+    End Property
+
+    Public ReadOnly LastAccessTimeUtc As DateTime
+
+    Public ReadOnly Property LastWriteTime As DateTime
+        Get
+            Return Me.LastWriteTimeUtc.ToLocalTime()
+        End Get
+    End Property
+
+    Public ReadOnly LastWriteTimeUtc As DateTime
+    Public ReadOnly Size As Long
+    Public ReadOnly Name As String
+    Public ReadOnly Path As String
+
+    Public Overrides Function ToString() As String
+        Return Me.Name
+    End Function
+
+    Friend Sub New(ByVal dir As String, ByVal findData As WIN32_FIND_DATA)
+        Me.Attributes = findData.dwFileAttributes
+        Me.CreationTimeUtc = ConvertDateTime(findData.ftCreationTime_dwHighDateTime, findData.ftCreationTime_dwLowDateTime)
+        Me.LastAccessTimeUtc = ConvertDateTime(findData.ftLastAccessTime_dwHighDateTime, findData.ftLastAccessTime_dwLowDateTime)
+        Me.LastWriteTimeUtc = ConvertDateTime(findData.ftLastWriteTime_dwHighDateTime, findData.ftLastWriteTime_dwLowDateTime)
+        Me.Size = CombineHighLowInts(findData.nFileSizeHigh, findData.nFileSizeLow)
+        Me.Name = findData.cFileName
+        Me.Path = IO.Path.Combine(dir, findData.cFileName)
+    End Sub
+
+    Private Shared Function CombineHighLowInts(ByVal high As UInteger, ByVal low As UInteger) As Long
+        Return (CLng(high) << &H20) Or low
+    End Function
+
+    Private Shared Function ConvertDateTime(ByVal high As UInteger, ByVal low As UInteger) As DateTime
+        Dim fileTime As Long = CombineHighLowInts(high, low)
+        Return DateTime.FromFileTimeUtc(fileTime)
+    End Function
+End Class
+
+
+
+<Serializable, StructLayout(LayoutKind.Sequential, CharSet:=CharSet.Auto), BestFitMapping(False)>
+Friend Class WIN32_FIND_DATA
+    Public dwFileAttributes As FileAttributes
+    Public ftCreationTime_dwLowDateTime As UInteger
+    Public ftCreationTime_dwHighDateTime As UInteger
+    Public ftLastAccessTime_dwLowDateTime As UInteger
+    Public ftLastAccessTime_dwHighDateTime As UInteger
+    Public ftLastWriteTime_dwLowDateTime As UInteger
+    Public ftLastWriteTime_dwHighDateTime As UInteger
+    Public nFileSizeHigh As UInteger
+    Public nFileSizeLow As UInteger
+    Public dwReserved0 As Integer
+    Public dwReserved1 As Integer
+    <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=260)>
+    Public cFileName As String
+    <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=14)>
+    Public cAlternateFileName As String
+
+    Public Overrides Function ToString() As String
+        Return "File name=" & cFileName
+    End Function
+End Class
+
+
+
+Module FastDirectoryEnumerator
+
+    Public Function EnumerateFiles(ByVal path As String) As IEnumerable(Of FileData)
+        Return FastDirectoryEnumerator.EnumerateFiles(path, "*")
+    End Function
+
+    Public Function EnumerateFiles(ByVal path As String, ByVal searchPattern As String) As IEnumerable(Of FileData)
+        Return FastDirectoryEnumerator.EnumerateFiles(path, searchPattern, SearchOption.TopDirectoryOnly)
+    End Function
+
+    Function EnumerateFiles(ByVal thePath As String, ByVal searchPattern As String, ByVal searchOption As SearchOption) As IEnumerable(Of FileData)
+        If thePath Is Nothing Then
+            Throw New ArgumentNullException(NameOf(thePath))
+        End If
+
+        If searchPattern Is Nothing Then
+            Throw New ArgumentNullException(NameOf(searchPattern))
+        End If
+
+        If (searchOption <> SearchOption.TopDirectoryOnly) AndAlso (searchOption <> SearchOption.AllDirectories) Then
+            Throw New ArgumentOutOfRangeException(NameOf(searchOption))
+        End If
+
+        Dim fullPath As String = Path.GetFullPath(thePath)
+        Return New FileEnumerable(fullPath, searchPattern, searchOption)
+    End Function
+
+    Function GetFiles(ByVal path As String, ByVal searchPattern As String, ByVal searchOption As SearchOption) As FileData()
+        Dim e As IEnumerable(Of FileData) = FastDirectoryEnumerator.EnumerateFiles(path, searchPattern, searchOption)
+        Dim list As List(Of FileData) = New List(Of FileData)(e)
+        Dim retval As FileData() = New FileData(list.Count - 1) {}
+        list.CopyTo(retval)
+        Return retval
+    End Function
+
+
+
+    Private Class FileEnumerable
+        Implements IEnumerable(Of FileData)
+
+        Private ReadOnly m_path As String
+        Private ReadOnly m_filter As String
+        Private ReadOnly m_searchOption As SearchOption
+
+        Public Sub New(ByVal path As String, ByVal filter As String, ByVal searchOption As SearchOption)
+            m_path = path
+            m_filter = filter
+            m_searchOption = searchOption
+        End Sub
+
+        Public Function GetEnumerator() As IEnumerator(Of FileData) Implements IEnumerable(Of FileData).GetEnumerator
+            Return New FileEnumerator(m_path, m_filter, m_searchOption)
+        End Function
+
+        Private Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+            Return New FileEnumerator(m_path, m_filter, m_searchOption)
+        End Function
+    End Class
+
+
+
+    Private NotInheritable Class SafeFindHandle
+        Inherits SafeHandleZeroOrMinusOneIsInvalid
+
+        <ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)>
+        <DllImport("kernel32.dll")>
+        Private Shared Function FindClose(ByVal handle As IntPtr) As Boolean
+
+        End Function
+
+        <SecurityPermission(SecurityAction.LinkDemand, UnmanagedCode:=True)>
+        Friend Sub New()
+            MyBase.New(True)
+        End Sub
+
+        Protected Overrides Function ReleaseHandle() As Boolean
+            Return FindClose(MyBase.handle)
+        End Function
+    End Class
+
+
+
+    <System.Security.SuppressUnmanagedCodeSecurity>
+    Private Class FileEnumerator
+        Implements IEnumerator(Of FileData)
+
+        <DllImport("kernel32.dll", CharSet:=CharSet.Auto, SetLastError:=True)>
+        Private Shared Function FindFirstFile(<MarshalAs(UnmanagedType.LPWStr)> fileName As String, <[In], Out> data As WIN32_FIND_DATA) As SafeFindHandle
+        End Function
+
+        <DllImport("kernel32.dll", CharSet:=CharSet.Auto, SetLastError:=True)>
+        Private Shared Function FindNextFile(hndFindFile As SafeFindHandle, <[In], Out, MarshalAs(UnmanagedType.LPStruct)> lpFindFileData As WIN32_FIND_DATA) As Boolean
+        End Function
+
+
+
+        Private Class SearchContext
+            Public ReadOnly Path As String
+            Public SubdirectoriesToProcess As Stack(Of String)
+
+            Public Sub New(ByVal path As String)
+                Me.Path = path
+            End Sub
+        End Class
+
+
+
+        Private m_path As String
+        Private ReadOnly m_filter As String
+        Private ReadOnly m_searchOption As SearchOption
+        Private ReadOnly m_contextStack As Stack(Of SearchContext)
+        Private m_currentContext As SearchContext
+        Private m_hndFindFile As SafeFindHandle
+        Private ReadOnly m_win_find_data As WIN32_FIND_DATA = New WIN32_FIND_DATA()
+
+        Public Sub New(ByVal path As String, ByVal filter As String, ByVal searchOption As SearchOption)
+            m_path = path
+            m_filter = filter
+            m_searchOption = searchOption
+            m_currentContext = New SearchContext(path)
+
+            If m_searchOption = SearchOption.AllDirectories Then
+                m_contextStack = New Stack(Of SearchContext)()
+            End If
+        End Sub
+
+        Public ReadOnly Property Current As FileData Implements IEnumerator(Of FileData).Current
+            Get
+                Return New FileData(m_path, m_win_find_data)
+            End Get
+        End Property
+
+        Private ReadOnly Property IEnumerator_Current As Object Implements IEnumerator.Current
+            Get
+                Return Current
+            End Get
+        End Property
+
+        Public Sub Dispose() Implements IDisposable.Dispose
+            If m_hndFindFile IsNot Nothing Then
+                m_hndFindFile.Dispose()
+            End If
+        End Sub
+
+        Public Function MoveNext() As Boolean Implements IEnumerator.MoveNext
+            Dim retval As Boolean = False
+
+            If m_currentContext.SubdirectoriesToProcess Is Nothing Then
+
+                If m_hndFindFile Is Nothing Then
+                    With New FileIOPermission(FileIOPermissionAccess.PathDiscovery, m_path)
+                        .Demand()
+                    End With
+
+                    Dim searchPath As String = Path.Combine(m_path, m_filter)
+                    m_hndFindFile = FindFirstFile(searchPath, m_win_find_data)
+                    retval = Not m_hndFindFile.IsInvalid
+                Else
+                    retval = FindNextFile(m_hndFindFile, m_win_find_data)
+                End If
+            End If
+
+            If retval Then
+                If (CType(m_win_find_data.dwFileAttributes, FileAttributes) And FileAttributes.Directory) = FileAttributes.Directory Then
+                    Return MoveNext()
+                End If
+            ElseIf m_searchOption = SearchOption.AllDirectories Then
+
+                If m_currentContext.SubdirectoriesToProcess Is Nothing Then
+                    Dim subDirectories As String() = Directory.GetDirectories(m_path)
+                    m_currentContext.SubdirectoriesToProcess = New Stack(Of String)(subDirectories)
+                End If
+
+                If m_currentContext.SubdirectoriesToProcess.Count > 0 Then
+                    Dim subDir As String = m_currentContext.SubdirectoriesToProcess.Pop()
+                    m_contextStack.Push(m_currentContext)
+                    m_path = subDir
+                    m_hndFindFile = Nothing
+                    m_currentContext = New SearchContext(m_path)
+                    Return MoveNext()
+                End If
+
+                If m_contextStack.Count > 0 Then
+                    m_currentContext = m_contextStack.Pop()
+                    m_path = m_currentContext.Path
+
+                    If m_hndFindFile IsNot Nothing Then
+                        m_hndFindFile.Close()
+                        m_hndFindFile = Nothing
+                    End If
+
+                    Return MoveNext()
+                End If
+            End If
+
+            Return retval
+        End Function
+
+        Public Sub Reset() Implements IEnumerator.Reset
+            m_hndFindFile = Nothing
+        End Sub
+    End Class
+End Module
--- README.md
+++ README.md
@@ -1,2 +1,145 @@
 # FastDirectoryEnumerator-VB.NET
  VB.NET version of FastDirectoryEnumerator by wilsone8
+
+### Original Description
+
+A Faster Directory Enumerator
+Author:  wilsone8
+Date:    2009 Aug 27
+License: CPOL
+Describes how to create a significantly faster enumerator for the attributes of all the files in a directory.
+
+## Introduction
+
+The .NET Framework's Directory class includes methods for querying the list of files in a directory. For each file, you can also then query the attributes of the file, such as the size, creation date, etc. However, when querying files on a remote PC, this can be very inefficient; a potentially expensive network round-trip is needed to retrieve each file's attributes. This article describes a much more efficient implementation that is approximately 3x faster.
+
+## Background
+
+Let's assume you are writing an application that needs to find the most recently modified file in a directory. To implement this, you might have a function similar to the following:
+
+    DateTime GetLastFileModifiedSlow(string dir)
+    {
+        DateTime retval = DateTime.MinValue;
+        
+        string [] files = Directory.GetFiles(dir);
+        for (int i=0; i<files.Length; i++)
+        {
+            DateTime lastWriteTime = File.GetLastWriteTime(files[i]);
+            if (lastWriteTime > retval)
+            {
+                retval = lastWriteTime;
+            }
+        }
+        
+        return retval;
+    }
+
+That function certainly works, but it suffers from some very poor performance characteristics:
+
+  -  GetFiles must allocate a potentially very large array.
+  -  GetFiles must wait for the entire directory's entries to be returned before returning.
+  -  For each file, a potentially expensive query is sent to the file system. No attempt is made to perform any sort of batch query.
+
+You might think that converting to DirectoryInfo.GetFileSystemInfos would improve item #3:
+
+    DateTime GetLastFileModifiedSlow2(string dir)
+    {
+        DateTime retval = DateTime.MinValue;
+        
+        DirectoryInfo dirInfo = new DirectoryInfo(dir);
+
+        FileInfo[] files = dirInfo.GetFiles();
+        for (int i=0; i<files.Length; i++)
+        {
+            if (files[i].LastWriteTime > retval)
+            {
+                retval = lastWriteTime;
+            }
+        }
+        
+        return retval;
+    }
+
+This doesn't change anything however: the objects returned by GetFiles() are not initialized with any data, and will all query the file system the first time any property is accessed.
+
+## Making it Faster
+
+The attached test application includes the FastDirectoryEnumerator class in FastDirectoryEnumerator.cs. Using the GetFiles method, we can write the equivalent of our first slow method.
+
+    DateTime GetLastFileModifiedFast(string dir)
+    {
+        DateTime retval = DateTime.MinValue;
+        
+        FileData [] files = FastDirectoryEnumerator.GetFiles(dir);
+        for (int i=0; i<files.Length; i++)
+        {
+            if (files[i].LastWriteTime > retval)
+            {
+                retval = lastWriteTime;
+            }
+        }
+        
+        return retval;
+    }
+
+The FileData object provides all the standard attributes for a file that the FileInfo class provides.
+
+## Making it Even Faster
+
+Use one of the overloads of the EnumerateFiles method to enumerate over all the files in a directory. The enumeration returns a FileData object.
+
+Below is an example of the same method using FastDirectoryEnumerator:
+
+    DateTime GetLastFileModifiedFast(string dir)
+    {
+        DateTime retval = DateTime.MinValue;
+
+        foreach (FileData f in FastDirectoryEnumerator.EnumerateFiles(dir))
+        {
+            if (f.LastWriteTime > retval)
+            {
+                retval = f.LastWriteTime;
+            }
+        }
+
+        return retval;
+    }
+
+## Performance
+
+The test application allows you to create a large number of files in a directory, then test the time it takes to enumerate using all three methods. I used a directory with 3000 files and ran each test three times to give the best answer possible for each test.
+
+Using a path on my local hard drive resulted in the following times:
+
+  -  Directory.GetFiles method: ~225ms
+  -  DirectoryInfo.GetFiles method: ~230ms
+  -  FastDirectoryEnumerator.GetFiles method: ~33ms
+  -  FastDirectoryEnumerator.EnumerateFiles method: ~27ms
+
+That is roughly a 8.5x increase in performance between the fastest and the slowest methods. The performance is even more pronounced when the files are on a UNC path. For this test, I used the same directory as the previous test. The only difference is that I referenced the directory by a UNC share name instead of the local path. At the time of the test, I was connected to my home wireless network.
+
+  -  Directory.GetFiles method: ~43,860ms
+  -  DirectoryInfo.GetFiles method: ~44,000ms
+  -  FastDirectoryEnumerator.GetFiles method: ~55ms
+  -  FastDirectoryEnumerator.EnumerateFiles method: ~53ms
+
+That is roughly a 830x increase in performance, and more than 2 orders of magnitude! And, the gap only increases as the latency to the PC containing the files increases.
+
+## Why is it Faster?
+
+As mentioned above, Directory.GetFiles and DirectoryInfo.GetFiles have a number of disadvantages. The most significant is that they throw away information and do not efficiently allow you to retrieve information about multiple files at the same time.
+
+Internally, Directory.GetFiles is implemented as a wrapper over the Win32 FindFirstFile/FindNextFile functions. These functions all return information about each file that is enumerated that the GetFiles() method throws away when it returns the file names. They also retrieve information about multiple files with a single network message.
+
+The FastDirectoryEnumerator keeps this information and returns it in the FileData class. This substantially reduces the number of network round-trips needed to accomplish the same task.
+
+## History
+
+  -  8-13-2009: Initial version.
+  -  8-14-2009: Added security checks, parameter checking, and the GetFiles method.
+  -  8-24-2009: Fixed the AllDirectories search using GetFiles. Removed note about .NET 4.0 including something similar.
+  -  9-08-2009: Fixed the AllDirectories search when filter is not * or *.*.
+
+## License
+
+This article, along with any associated source code and files, is licensed under [The Code Project Open License (CPOL)](http://www.codeproject.com/info/cpol10.aspx)
